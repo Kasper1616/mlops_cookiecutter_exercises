ad283eeef2fabc8371d46d0bd8bca4209b202291
s214786 <s214786@student.dtu.dk>
2025-01-07 11:11:33 +0100

    All relevant files in correct structure using typer and more

--- src/s2_cookiecutter/data.py
+++ src/s2_cookiecutter/data.py
@@ -2,28 +2,46 @@ from pathlib import Path
 
 import typer
 from torch.utils.data import Dataset
+import torch
 
+def normalize(images: torch.Tensor) -> torch.Tensor:
+    return (images - images.mean())/images.std()
+    
+def preprocess_data(raw_dir: str = "data/raw", processed_dir: str = "data/processed") -> None:
+    train_images, train_target = [], []
+    for i in range(6):
+        train_images.append(torch.load(f"{raw_dir}/train_images_{i}.pt"))
+        train_target.append(torch.load(f"{raw_dir}/train_target_{i}.pt"))
+    train_images = torch.cat(train_images)
+    train_target = torch.cat(train_target)
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+    test_images: torch.Tensor = torch.load(f"{raw_dir}/test_images.pt")
+    test_target: torch.Tensor = torch.load(f"{raw_dir}/test_target.pt")
 
-    def __init__(self, raw_data_path: Path) -> None:
-        self.data_path = raw_data_path
+    train_images = train_images.unsqueeze(1).float()
+    test_images = test_images.unsqueeze(1).float()
+    train_target = train_target.long()
+    test_target = test_target.long()
 
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
+    train_images = normalize(train_images)
+    test_images = normalize(test_images)
 
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
+    torch.save(train_images, f"{processed_dir}/train_images.pt")
+    torch.save(train_target, f"{processed_dir}/train_target.pt")
+    torch.save(test_images, f"{processed_dir}/test_images.pt")
+    torch.save(test_target, f"{processed_dir}/test_target.pt")
 
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
+def corrupt_mnist() -> tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+    train_images = torch.load("data/processed/train_images.pt")
+    train_target = torch.load("data/processed/train_target.pt")
+    test_images = torch.load("data/processed/test_images.pt")
+    test_targets = torch.load("data/processed/test_target.pt")
 
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    dataset = MyDataset(raw_data_path)
-    dataset.preprocess(output_folder)
+    train_set = torch.utils.data.TensorDataset(train_images, train_target)
+    test_set = torch.utils.data.TensorDataset(test_images, test_targets)
 
+    return train_set, test_set
+ 
 
 if __name__ == "__main__":
-    typer.run(preprocess)
+    typer.run(preprocess_data())
